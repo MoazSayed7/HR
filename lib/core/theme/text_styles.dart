a8e79153e9e39f2785a39d03f249ced7902bc84e
Moaz Sayed <moazsaid7586@gmail.com>
2025-03-14 19:29:19 +0200

    - install needed dependencies - separate main file and app class - create home screen - create TextStyles helper Class - create Font Weight helper class - create Colors Manager helper class - make AppRouter - make Shared Preference helper class - upgrade MinSDK to 23 - upgrade compileSDK to 35

---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'font_weight_helper.dart';
+
+class TextStyles {
+  static TextStyle font17WhiteSemiBold = TextStyle(
+    fontSize: 17.sp,
+    fontWeight: FontWeightHelper.semiBold,
+    color: Colors.white,
+  );
+}
